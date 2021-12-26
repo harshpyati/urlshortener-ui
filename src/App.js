3bbbd9e48cec5659b9a15b80b9bb4c26aaf26191
Harsh Pyati <harshvivek802@gmail.com>
2021-12-27 00:02:08 +0530

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const [url, setUrl] = useState(null);
+  const [shortenedUrl, setShortenedUrl] = useState(null)
+  const [duration, setDuration] = useState(3600000)
+  const [redirect,setRedirect] = useState(null);
+
+  const shortenUrl = () => {
+    const reqBody = {
+      "originalUrl": url,
+      "requiredDuration": duration
+    }
+
+    fetch("http://localhost:8080/shorten", {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(reqBody)
+    }).then(res => res.json())
+      .then(data => {
+        setShortenedUrl(data["shortenedUrl"])
+        setRedirect("http://localhost:8080/redirect?id=" + data["shortenedUrl"].split(".com/")[1])
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <Header />
+      <input type={"text"} onChange={(e) => setUrl(e.target.value)} />
+      <p>
+        <a href={redirect} target={"_blank"} rel={"external"}>
+          {shortenedUrl}
         </a>
-      </header>
+      </p>
+      <select onChange={(e) => setDuration(e.target.value)}>
+        <option value={3600000}>1 hour</option>
+        <option value={86400000}>1 day</option>
+      </select>
+      <button onClick={shortenUrl}>
+        Shorten
+      </button>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            urlr : the open source url shortener
+        </header>
+    )
+}
+
+export default Header;

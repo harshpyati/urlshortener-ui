ab71ff321911d97ad03808f8e137da1d2cc6db89
Harsh Pyati <harshvivek802@gmail.com>
2022-01-08 01:54:34 +0530

    added styles to the app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [url, setUrl] = useState(null);
   const [shortenedUrl, setShortenedUrl] = useState(null)
   const [duration, setDuration] = useState(3600000)
-  const [redirect,setRedirect] = useState(null);
+  const [redirect, setRedirect] = useState(null);
 
   const shortenUrl = () => {
     const reqBody = {
@@ -23,8 +23,9 @@ function App() {
       body: JSON.stringify(reqBody)
     }).then(res => res.json())
       .then(data => {
-        setShortenedUrl(data["shortenedUrl"])
-        setRedirect("http://localhost:8080/redirect?id=" + data["shortenedUrl"].split(".com/")[1])
+        const { shortenedUrl } = data;
+        setShortenedUrl(shortenedUrl)
+        setRedirect("http://localhost:8080/redirect?id=" + shortenedUrl.split(".com/")[1])
       }).catch(err => {
         console.log(err);
       })
@@ -33,19 +34,27 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <input type={"text"} onChange={(e) => setUrl(e.target.value)} />
-      <p>
-        <a href={redirect} target={"_blank"} rel={"external"}>
-          {shortenedUrl}
-        </a>
-      </p>
-      <select onChange={(e) => setDuration(e.target.value)}>
-        <option value={3600000}>1 hour</option>
-        <option value={86400000}>1 day</option>
-      </select>
-      <button onClick={shortenUrl}>
-        Shorten
-      </button>
+      <div>
+        <input className='urlinput' type={"text"} onChange={(e) => setUrl(e.target.value)} placeholder='www.url.com' />
+      </div>
+      <div>
+        <p>
+          <a href={redirect} target={"_blank"} rel={"external"} className='shortenedurl'>
+            {shortenedUrl}
+          </a>
+        </p>
+      </div>
+      <div>
+        <select className='durationSelector' onChange={(e) => setDuration(e.target.value)}>
+          <option value={3600000}>1 hour</option>
+          <option value={86400000}>1 day</option>
+        </select>
+      </div>
+      <div>
+        <button className='shorten' onClick={shortenUrl}>
+          Shorten
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import '../styles/Header.css';
 const Header = () => {
     return (
-        <header>
-            urlr : the open source url shortener
-        </header>
+        <div>
+            <header>
+                urlr : an open source url shortener
+            </header>
+        </div>
+
     )
 }
 
